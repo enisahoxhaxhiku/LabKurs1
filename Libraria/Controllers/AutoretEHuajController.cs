305bd78a235a69885ee231e76e203cf531eedf51
adeamulaku <93317760+adeamulaku@users.noreply.github.com>
2022-11-13 23:57:28 +0100

    autoret_huaj controller

---
+++ Libraria/Controllers/AutoretEHuajController.cs
@@ -0,0 +1,135 @@
+﻿using Librat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Librat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoretEHuajController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public AutoretEHuajController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select AutoretHId, Emri, Mbiemri, Biografia from Autoret_Huaj";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"select * from Autoret_Huaj where AutoretHId = " + id + @"";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(AutoretEHuaj ah)
+        {
+            string query = @"insert into Autoret_Huaj values ('" + ah.Emri + @"', '" + ah.Mbiemri + @"', '" + ah.Biografia + @"')";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte shtuar me sukses.");
+        }
+
+        [HttpPut]
+        public JsonResult Put(AutoretEHuaj ah)
+        {
+            string query = @"update Autoret_Huaj set Emri = '" + ah.Emri + @"', Mbiemri = '" + ah.Mbiemri + @"', Biografia = '" + ah.Biografia + @"' where AutoretHId = " + ah.AutoretHId + @"";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte ndryshuar me sukses.");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from Autoret_Huaj where AutoretHId = " + id + @"";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte fshire me sukses.");
+        }
+    }
+}
