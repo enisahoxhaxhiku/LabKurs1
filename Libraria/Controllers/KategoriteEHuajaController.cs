e9c02c2bde7dbb8c8b207e410bf9df5402de1cde
adeamulaku <93317760+adeamulaku@users.noreply.github.com>
2022-11-13 23:59:03 +0100

    kategorite e librit

---
+++ Libraria/Controllers/KategoriteEHuajaController.cs
@@ -0,0 +1,143 @@
+﻿using Librat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+namespace Librat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KategoriteEHuajaController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public KategoriteEHuajaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"select KategoriaHID, Kategoria from Kategorite_Huaj";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+
+            SqlDataReader kategoriteReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, myCon))
+                {
+                    kategoriteReader = cmd.ExecuteReader();
+                    table.Load(kategoriteReader);
+
+                    kategoriteReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpPost]
+
+        public JsonResult Post(KategoriteEHuaja kategorite)
+        {
+            string query = @"insert into Kategorite_Huaj values ('" + kategorite.Kategoria + @"')";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+
+            SqlDataReader kategoriteReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, myCon))
+                {
+                    kategoriteReader = cmd.ExecuteReader();
+                    table.Load(kategoriteReader);
+                    
+                    kategoriteReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Shtuar me sukses!");
+
+        }
+
+
+        [HttpPut]
+
+        public JsonResult Put(Kategorite_Huaj kategorite)
+        {
+            string query = @"update Kategorite_Huaj set Kategoria = '" + kategorite.Kategoria + @"' where KategoriaHID = " + kategorite.KategoriaHID + @"";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+
+            SqlDataReader kategoriteReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, myCon))
+                {
+                    kategoriteReader = cmd.ExecuteReader();
+                    table.Load(kategoriteReader);
+
+                    kategoriteReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Ndryshuar me sukses!");
+        }
+
+        [HttpDelete("{kID}")]
+
+        public JsonResult Delete(int kID) {
+
+            string query = @"delete from Kategorite_Huaj where KategoriaHID = " + kID + @"";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+
+            SqlDataReader kategoriteReader;
+
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using(SqlCommand cmd = new SqlCommand(query, myCon))
+                {
+                    kategoriteReader=cmd.ExecuteReader();
+                    table.Load(kategoriteReader);
+
+                    kategoriteReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Fshire me sukses");
+        
+        }
+
+
+
+
+    }
+}
---
+++ Libraria/Controllers/KategoriteShqipController.cs
@@ -0,0 +1,112 @@
+﻿using Librat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Librat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KategoriteShqipController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public KategoriteShqipController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select KategoriaShId, Kategoria from Kategorite_Shqip";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(KategoriteShqip cat)
+        {
+            string query = @"insert into Kategorite_Shqip values ('" + cat.Kategoria + @"')";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte shtuar me sukses.");
+        }
+
+        [HttpPut]
+        public JsonResult Put(KategoriteShqip cat)
+        {
+            string query = @"update Kategorite_Shqip set Kategoria = '" + cat.Kategoria + @"' where KategoriaShId = " + cat.KategoriaShId + @"";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte ndryshuar me sukses.");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from Kategorite_Shqip where KategoriaShId = " + id + @"";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte fshire me sukses.");
+        }
+    }
+}
