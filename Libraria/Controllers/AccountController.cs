d0f9b6b9ffe9186280759695a5d9c9d915cd8a28
adeamulaku <93317760+adeamulaku@users.noreply.github.com>
2022-11-13 23:54:07 +0100

    account controller

---
+++ Libraria/Controllers/AccountController.cs
@@ -0,0 +1,298 @@
+﻿using Librat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Librat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Ky user Ekziston!" });
+
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Regjistrimi deshtoj, ju lutemi provoni me vone." });
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.User))
+            {
+                await _userManager.AddToRoleAsync(user, UserRoles.User);
+            }
+            return Ok(new Response { Status = "Success", Message = "Jeni regjistruar me sukses!" });
+        }
+
+
+        [HttpPost]
+        [Route("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Admin Ekziston!" });
+
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Regjistrimi i adminit Deshtoj, Provoni perseri" });
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+            }
+            return Ok(new Response { Status = "Success", Message = "Keni regjistruar adminin me sukses!" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = GetToken(authClaims);
+
+                return Ok(new
+                {
+                    id = user.Id,
+                    name = user.UserName,
+                    Email = user.Email,
+                    Role = userRoles,
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+ /*       [HttpGet, Authorize]
+        [Route("GetUser")]
+        public async Task<ActionResult<User>> GetCurrentUser()
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.FindFirstValue(ClaimTypes.Name));
+
+            if (user == null) return NotFound();
+
+            return CreateUserObject(user);
+        }
+
+        private User CreateUserObject(IdentityUser user)
+        {
+            return new User
+            {
+               Id = user.Id,
+               Name = user.UserName,
+               Email = user.Email,
+               Role = _roleService.GetMyRole()
+            };
+        }*/
+
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT ANR.Name,ANU.Email,ANU.UserName, ANU.NormalizedUserName, ANUR.UserId,ANUR.RoleId
+                                FROM AspNetRoles ANR
+                                LEFT OUTER JOIN AspNetUserRoles ANUR
+                                ON ANR.Id=ANUR.RoleId
+                                LEFT OUTER JOIN AspNetUsers ANU
+                                ON ANU.Id=ANUR.UserId
+                                ORDER BY 3";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+
+
+
+        [Route("GetRole")]
+        [HttpGet]
+        public JsonResult GetRole()
+        {
+            string query = @"select ANR.Id, ANR.Name from AspNetRoles ANR";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+
+
+
+
+
+
+        [HttpPut]
+        public JsonResult Put(UpdatesRole rol)
+        {
+            string query = @"UPDATE ANUR
+                          SET ANUR.RoleId = '" + rol.Id + @"'
+                          FROM AspNetUserRoles ANUR
+                          INNER JOIN AspNetRoles ANR
+                          ON ANUR.RoleId=ANR.Id
+                          INNER JOIN AspNetUsers ANU
+                          ON ANU.Id=ANUR.UserId
+                          WHERE ANU.NormalizedUserName='" + rol.emri + @"'";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader skenarReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    skenarReader = myCommand.ExecuteReader();
+                    table.Load(skenarReader); ;
+
+                    skenarReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte ndryshuar me sukses.");
+        }
+
+
+        [HttpDelete("{emri}")]
+        public JsonResult Delete(string emri)
+        {
+            string query = @"delete ANUR
+                          FROM AspNetRoles ANR
+                          full JOIN AspNetUserRoles ANUR
+                          ON ANR.Id=ANUR.RoleId
+                          full JOIN AspNetUsers ANU
+                          ON ANU.Id=ANUR.UserId
+                          where ANR.Name is not null and ANU.UserName = '" + emri + @"'";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LibratCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Eshte fshire me sukses.");
+        }
+
+    }
+
+
+
+  
+
+}
